99a9cb8a5909b06991deaa3193ff1edd0678ee8a
Alexis Smith <aj14smit@siena.edu>
2024-02-28 16:50:50 -0500

    Got the layout of the factulty page descriptions to be a little better

--- public_html/faculty_functions.php
+++ public_html/faculty_functions.php
@@ -17,74 +17,67 @@
 
 */
 function displayFaculty(){
-	$output = '';
+    $output = '';
 
-	$row = 0;
-	$output .= '
-	<div class="row">
-	';
-	if (($handle = fopen("faculty.csv", "r")) !== FALSE) {
-		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			if($row%3==0){
-				$output .='
-				</div>
-				<div class="row">
-				';
-			}
-			$row++;
-			$output.= '
-			<div class="col-md-4 '.$data[1].'">
-				<div class="panel panel-default">';
+    $row = 0;
+    $output .= '
+    <div class="row">
+    ';
+    if (($handle = fopen("faculty.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            if($row%3==0){
+                $output .='
+                </div>
+                <div class="row">
+                ';
+            }
+            $row++;
+            $output.= '
+            <div class="col-md-4 '.$data[1].'">
+                <div class="panel panel-default">';
+            // Displaying the Faculty Name and Title under the panel-heading
+            $output .= '
+                <div class="panel-heading text-center">
+                    <h3>'.$data[0].'</h3>
+                    <h4>'.$data[1].'</h4>
+                </div>';
+            // Displaying the Image if available
+            if($data[2]!= ''){
+                $output .='
+                <div class="panel-body">
+                    <img src="images/faculty/'.$data[2].'" alt="'.$data[0].'" height="500" class="center-block img-rounded" >
+                </div>';
+            }
+            // Displaying the Panel Footer
+            $output .= '
+                <div class="panel-footer">
+                    <div class="text-center">';
+            // Displaying "Department Chair" if available
+            if(isset($data[6]) && !empty($data[6])) {
+                $output .= '<h4>Department Chair</h4>';
+            }
+            // Displaying additional information if available
+            for ($i = 4; $i < count($data); $i++) {
+                if (!empty($data[$i])) {
+                    $output .= '<p>' . $data[$i] . '</p>';
+                }
+            }
+            $output .= '
+                    </div>
+                </div>
+            </div>
+        </div>';
+        }
+        fclose($handle);
+    }
 
-			$output .='
-			<div class="panel-heading text-center">
-			<h3>'.$data[0].'</h3>
-			';
+    //closing row div
+    $output .= '
+    </div>';
 
-			$output.='
-			</div>
-			';
-			$output.='';
-			if($data[2]!= ''){
-				$output .=' <div class="panel-body">
-				<img src="images/faculty/'.$data[2].'" alt = "'.$data[0].'" height="500" class="center-block img-rounded" >
-				</div>
-				';
-			}
-
-			$output .= '
-					<div class="panel-footer">
-							<div class="text-center">
-						<h4>'.$data[1].'</h4>
-						';
-			if($data[3]!=''){
-				$output.='
-				<h4>Department Chair</h4>
-				';
-			}
-
-			$output.='
-							</div>
-					</div>
-				  </div>
-				</div>
-
-			';
-
-		}
-		fclose($handle);
-
-	}
-
-	//closing row div
-	$output .= '
-	</div>
-	';
-
-
-
-
-	return $output;
+    return $output;
 }
+
+
 
 ?>
